8752829c4a7a786e9eca69c6d0d1ecd9f41be527
Philou <ph.bovey@gmail.com>
2016-05-11 23:45:44 +0200

    Update Affichage_7_seg.vhd

--- Affichage_7_seg.vhd
+++ Affichage_7_seg.vhd
@@ -1,75 +1,56 @@
 -----------------------------------------------------------------------------------//
--- Nom du projet 		    : Additionneur
--- Nom du fichier 		    : conversion_log_num.vhd
--- Date de création 	    : 16.03.2016
--- Date de modification     : xx.xx.2016
+-- Nom du projet 		    : xxx
+-- Nom du fichier 		    : aff_num_7seg.vhd
+-- Date de crÃ©ation 	            : 16.03.2016
+-- Date de modification             : 11.05.2016
 --
--- Auteur 				    : Ph. Bovey
+-- Auteur 			    : Ph. Bovey
 --
--- Description              : convertit une bus std_logic_vector en integer  
+-- Description                      : convertit une bus std_logic_vector en integer  
 --
 -- Remarques 			    :  
 -- 
--- lien 					: conversion 
+-- lien 			    : conversion 
 -- http://www.synthworks.com/papers/vhdl_math_tricks_mapld_2003.pdf
 ----------------------------------------------------------------------------------//
 ---------------------------------------------------------------
--- déclaration standart des librairies standart pour le VHDL --
+-- dÃ©claration standart des librairies standart pour le VHDL --
 --------------------------------------------------------------- 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL; 					-- pour les opérations mathématiques  
+use IEEE.NUMERIC_STD.ALL; 					-- pour les opÃ©rations mathÃ©matiques  
 
--------------------------------------------------
--- déclaration de l'entité (Entrées / Sorties) --
--------------------------------------------------
-entity AFF_7_SEG is
+-- dÃ©claration standart des librairies standart pour le VHDL 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- dÃ©claration de l'entite (EntrÃ©es / Sorties) 
+entity AFF_NUM_7SEG is
 	port(
-		------------
-		-- entrée --
-		------------ 
-		-- entrée simple -- 
+			
+		num_val : in integer range 0 to 15;
 		
-		-- entrée bus -- 
-		VAL_AFF : in integer range 0 to 100;			-- pour le sens de la lecture des switches (droite LSB Gauche MSB) 
-		
-		------------
-		-- sortie --
-		------------
-		-- sortie simple --  
-		SEGMENTS_A_TO_G : out std_logic_vector(6 downto 0)  
-		
-		-- sortie bus -- 
-
+		aff_valeurs_7seg : out std_logic_vector(6 downto 0)  
 	);
-END AFF_7_SEG;
+END AFF_NUM_7SEG;
 
-architecture COMPORTEMENT_AFF_7S of AFF_7_SEG is
-	
-	--------------------------------------------
-	-- déclaration de signaux, variables, etc --
-	--------------------------------------------
-	-- constante -- 
-	constant MAX_VALUE_CALCUL 	: integer := 100;
-	
+Architecture Comportement_Aff_Num_7Seg of AFF_NUM_7SEG is 	
 	begin
-	
-	with VAL_AFF select 
-						  --GFEDCBA--
-		SEGMENTS_A_TO_G <= "1000000" when 0,		-- 0
-						   "1111100" when 1,		
-						   "0010100" when 2,        
-						   "0110000" when 3,
-						   "0101001" when 4,
-						   "0100010" when 5,
-						   "0000010" when 6,
-						   "1111000" when 7,
-						   "0000000" when 8,
-						   "0100000" when 9,	
-						   "0010001" when 100,  	-- d comme dépassé 
-						   "0111111" when others; 	-- -
-	--with select
-	
-	
 
-end COMPORTEMENT_AFF_7S; 
+	aff_valeurs_7seg <= "1000000" when num_val = 0 else      -- doit afficher la valeur 0
+	                    "1111001" when num_val = 1 else      -- doit afficher la valeur 1
+	                    "0100100" when num_val = 2 else		 -- doit afficher la valeur 2
+	                    "0110000" when num_val = 3 else		 -- doit afficher la valeur 3
+	                    "0011001" when num_val = 4 else		 -- doit afficher la valeur 4
+	                    "0010010" when num_val = 5 else		 -- doit afficher la valeur 5
+	                    "0000010" when num_val = 6 else		 -- doit afficher la valeur 6
+	                    "1111000" when num_val = 7 else		 -- doit afficher la valeur 7
+	                    "0000000" when num_val = 8 else		 -- doit afficher la valeur 8
+	                    "0011000" when num_val = 9 else		 -- doit afficher la valeur 9
+	                    "0001000" when num_val = 10 else		 -- doit afficher la valeur A
+	                    "0000011" when num_val = 11 else		 -- doit afficher la valeur b
+	                    "0100111" when num_val = 12 else		 -- doit afficher la valeur c
+	                    "0100001" when num_val = 13 else		 -- doit afficher la valeur d
+	                    "0000110" when num_val = 14 else		 -- doit afficher la valeur E
+	                    "0001110" when num_val = 15; 			 -- doit afficher la valeur F 
+end Comportement_Aff_Num_7Seg; 
